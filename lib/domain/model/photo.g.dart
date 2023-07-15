63de2285fa6c016fb75dbed7d35f3ee42455f7e3
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-15 11:11:10 +0900

    feat: photo model 구현

---
+++ lib/domain/model/photo.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'photo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Photo _$PhotoFromJson(Map<String, dynamic> json) => Photo(
+      id: json['id'] as num,
+      url: json['url'] as String,
+      views: json['views'] as num,
+    );
+
+Map<String, dynamic> _$PhotoToJson(Photo instance) => <String, dynamic>{
+      'id': instance.id,
+      'url': instance.url,
+      'views': instance.views,
+    };
