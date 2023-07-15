2f790f939e1833a0ad53340bf632fbf1d9a0f6d4
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-15 11:13:31 +0900

    feat: router, provider 연결

---
+++ lib/core/router/router.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_search_10day/data/repository/pixabay_photo_repository_impl.dart';
+import 'package:image_search_10day/domain/model/photo.dart';
+import 'package:image_search_10day/presentation/detail/detail_screen.dart';
+import 'package:image_search_10day/presentation/main/main_screen.dart';
+import 'package:image_search_10day/presentation/main/main_view_model.dart';
+import 'package:provider/provider.dart';
+
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return ChangeNotifierProvider(
+          create: (_) => MainViewModel(
+            PixabayPhotoRepositoryImpl(),
+          ),
+          child: const MainScreen(),
+        );
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'detail',
+          builder: (BuildContext context, GoRouterState state) {
+            final photo = state.extra as Photo;
+            return DetailScreen(photo: photo);
+          },
+        ),
+      ],
+    ),
+  ],
+);
